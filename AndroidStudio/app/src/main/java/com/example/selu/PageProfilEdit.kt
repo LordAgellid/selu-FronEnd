187f524649a9a59d3d374383a164725c4acd6c0b
Salah Ouamalich <e2050624@cmaisonneuve.qc.ca>
2022-10-02 23:28:18 -0400

    page edit profil creation

---
+++ AndroidStudio/app/src/main/java/com/example/selu/PageProfilEdit.kt
@@ -0,0 +1,11 @@
+package com.example.selu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PageProfilEdit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.page_edit_profil)
+    }
+}
